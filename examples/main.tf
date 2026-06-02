7c6256852060a098a11c0629c42ea47a3ae318d4
Valentina Ochoa <val.ochoa@duocuc.cl>
2026-06-02 18:09:19 -0400

    fix: correct examples main.tf content

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,5 @@
-@'
-module "ec2" {
+﻿module "ec2" {
   source = "git::https://github.com/valentina-ochoa/terraform-aws-ec2-AUY1105-OT.git?ref=v0.2.0"
-
   ami_id              = "ami-0c55b159cbfafe1f0"
   instance_type       = "t2.micro"
   subnet_id           = "subnet-xxxxxxxxxxxxxxxxx"
@@ -10,12 +8,3 @@ module "ec2" {
   kms_deletion_window = 7
   log_retention_days  = 365
 }
-
-output "instance_id" {
-  value = module.ec2.instance_id
-}
-
-output "instance_ip" {
-  value = module.ec2.instance_ip
-}
-'@ | Out-File -FilePath "C:\Users\graci\terraform-aws-ec2-AUY1105-OT\examples\main.tf" -Encoding utf8 -Force
